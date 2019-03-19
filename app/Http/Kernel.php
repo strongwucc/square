9544589abf8a4d436231aca6aadc1572ca357456
wuchuanchuan <superwucc@gmail.com>
2019-03-19 09:34:54 +0000

    添加日志

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'change-locale' => \App\Http\Middleware\ChangeLocale::class,
+        'api-log' => \App\Http\Middleware\ApiLog::class,
     ];
 
     /**
