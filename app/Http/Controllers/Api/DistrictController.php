bc96b794566e828249490f6b6c0ce5c7b8d7b39c
lucienwu <superwucc@gmail.com>
2020-10-12 15:07:22 +0800

    海科短信通道接入

--- app/Http/Controllers/Api/DistrictController.php
+++ app/Http/Controllers/Api/DistrictController.php
@@ -6,6 +6,7 @@ use App\Models\O2oCouponBuy;
 use App\Models\O2oOrder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 use App\Jobs\SendSms;
 
