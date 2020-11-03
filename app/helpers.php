16ac99720951e931875fe845f7a6202d3b1a789d
lucienwu <superwucc@gmail.com>
2020-11-03 17:10:23 +0800

    接口测试

--- app/helpers.php
+++ app/helpers.php
@@ -215,16 +215,12 @@ function is_return_vaild($arr)
 
 function post_json($url,$data) {
 
-    $data_string = json_encode($data);
-
     $headers = array(
-        "Content-type: application/json;charset=UTF-8",
+        "Content-type: application/json;charset='utf-8'",
         "Accept: application/json",
         "Cache-Control: no-cache",
         "Pragma: no-cache",
-        'Content-Length: ' . strlen($data_string)
     );
-
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_TIMEOUT, 60); //设置超时
     if(0 === strpos(strtolower($url), 'https')) {
@@ -232,28 +228,8 @@ function post_json($url,$data) {
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); //从证书中检查SSL加密算法是否存在
     }
     curl_setopt($ch, CURLOPT_POST, TRUE);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    $return = curl_exec($ch);//CURLOPT_RETURNTRANSFER 不设置  curl_exec返回TRUE 设置  curl_exec返回json(此处) 失败都返回FALSE
-    curl_close($ch);
-
-    return $return;
-}
-
-function post_yancao($url,$data) {
-
-    $data_string = json_encode($data);
-
-    $headers = array(
-        "Content-type: application/json;charset=UTF-8",
-        'Content-Length: ' . strlen($data_string)
-    );
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $return = curl_exec($ch);//CURLOPT_RETURNTRANSFER 不设置  curl_exec返回TRUE 设置  curl_exec返回json(此处) 失败都返回FALSE
     curl_close($ch);
@@ -352,9 +328,8 @@ function yancao_query($data, &$msg){
         'signature' => md5($data['liceId'] . $api_key)
     );
 
-    Log::channel('api')->info('[烟草证号查询请求地址]：' . $api_url);
     Log::channel('api')->info('[烟草证号查询请求报文]：' . print_r($query_data, true));
-    $query_res = post_yancao($api_url, $query_data);
+    $query_res = post_json($api_url, $query_data);
     Log::channel('api')->info('[烟草证号查询响应报文]：' . print_r($query_res, true));
     $res_data = json_decode($query_res,true);
     if($res_data['ret_code'] == '0000'){
